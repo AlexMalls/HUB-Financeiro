c2ccfc920caccd7aeef24a11e82dc6808b5049f4
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-08-21 16:42:28 -0300

    Add files via upload

---
+++ models/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿public class Fornecedor
+{
+    public string Nome { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Telefone { get; set; } = string.Empty;
+    public string CNPJ { get; set; } = string.Empty;
+    public bool Ativo { get; set; }
+
+    public override string ToString()
+    {
+        return Nome;
+    }
+}
