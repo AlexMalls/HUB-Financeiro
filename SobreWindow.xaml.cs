23ddb9ebc7ea9c21f4a1dcafdf38a2615b0778df
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-08-26 17:51:35 -0300

    Persiste contextos OPEX e adiciona Infos Positiva e Sobre
    O.P.E.X. passa a persistir snapshots em JSON; adiciona Infos Positiva e Sobre com identidade Ennoia. Build completo validado em Windows/.NET 8.

---
+++ SobreWindow.xaml.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using System.Windows;
+using System.Windows.Input;
+
+namespace HubFinanceiro;
+
+public partial class SobreWindow : Window
+{
+    public SobreWindow()
+    {
+        InitializeComponent();
+
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        VersionText.Text = version == null
+            ? "Versão 1.0"
+            : $"Versão {version.Major}.{version.Minor}.{version.Build}";
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ButtonState == MouseButtonState.Pressed)
+            DragMove();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
