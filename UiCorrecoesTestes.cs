fc680ef0d42713b4d62315308b634f8a71261201
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-08-28 14:31:26 -0300

    test: confirma overflow das ações OPEX

--- UiCorrecoesTestes.cs
+++ UiCorrecoesTestes.cs
@@ -9,8 +9,8 @@ public static class UiCorrecoesTestes
         DeveConstruirTabelaFornecedoresMesmoComPainelEmailOculto();
         DeveAlinharLayoutComPesquisaFornecedores();
         DeveUsarEstruturaVisualDaTabelaOpexNosFornecedores();
-        DeveReservarRoxoParaEstadosAtivosNaTelaFornecedores();
         DeveManterAcoesOpexVisiveisEmLarguraCompacta();
+        DeveReservarRoxoParaEstadosAtivosNaTelaFornecedores();
         DeveIgnorarClientesCanceladosPorPadrao();
         DeveReservarLarguraSuficienteParaBotoesOpex();
     }
