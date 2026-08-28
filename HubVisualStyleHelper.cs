bff5f153cb8644d0720af0603fc83678418f5058
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-08-28 10:07:58 -0300

    Reutiliza estilos visuais do HUB em janelas auxiliares

---
+++ HubVisualStyleHelper.cs
@@ -0,0 +1,21 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HubFinanceiro;
+
+internal static class HubVisualStyleHelper
+{
+    public static void AplicarScrollBarPadrao(Window target)
+    {
+        var origem = Application.Current?.MainWindow;
+        if (origem?.TryFindResource(typeof(ScrollBar)) is Style style)
+            target.Resources[typeof(ScrollBar)] = style;
+    }
+
+    public static void AplicarCheckBoxPadrao(CheckBox checkBox)
+    {
+        var origem = Application.Current?.MainWindow;
+        if (origem?.TryFindResource("CustomCheckBoxStyle") is Style style)
+            checkBox.Style = style;
+    }
+}
